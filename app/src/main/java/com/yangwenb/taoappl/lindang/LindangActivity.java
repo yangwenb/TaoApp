9caf2fdb641e1d9dcf6ba1a9b1d70a666741a6e1
yangwenbin <huituxianshen@163.com>
2019-05-30 08:36:19 +0800

    完善消息中心的 toolbar

--- app/src/main/java/com/yangwenb/taoappl/lindang/LindangActivity.java
+++ app/src/main/java/com/yangwenb/taoappl/lindang/LindangActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 import com.yangwenb.taoappl.R;
 
@@ -22,5 +23,21 @@ public class LindangActivity extends AppCompatActivity {
     private void initView() {
         lin_tool = (Toolbar) findViewById(R.id.lin_tool);
         lin_rv = (RecyclerView) findViewById(R.id.lin_rv);
+
+        //设置tool
+        initTool();
+    }
+
+    //设置tool
+    private void initTool() {
+        lin_tool.setTitle("");
+        setSupportActionBar(lin_tool);
+        lin_tool.setNavigationIcon(R.drawable.fanhui);
+        lin_tool.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
